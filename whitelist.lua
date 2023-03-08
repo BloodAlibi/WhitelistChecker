8b8903d80d4c9c0be1a72dbc70873f8b6303010b
BloodAlibi <bloodalibi@tkium.com>
2023-03-08 10:19:55 -0500

    Main file update
    - No more CAPS variables
    - A few more comments

--- whitelist.lua
+++ whitelist.lua
@@ -1,11 +1,14 @@
-local PLAYERS = game:GetService("Players")
+-- For ROBLOX experiences
+-- in Server Script object
 
-local ACCOUNTS_IDS = {0,0,0} -- Accounts IDs
-local KICK_MESSAGE = "You were not in the game whitelist. Sorry!" -- Your kick message
+local players = game:GetService("Players")
 
-PLAYERS.PlayerAdded:Connect(function(PLAYER)
-	if not table.find(ACCOUNTS_IDS,PLAYER.UserId) then
-		print(PLAYER.Name.." attempted to join the game but was not in the whitelist.") -- The script prints a message to indicate to others that an unauthorized player attempted to enter
-		PLAYER:Kick(KICK_MESSAGE) -- The players is kicked
+local accounts = {0,0,0} -- Accounts IDs
+local kick_message = "You were not in the game whitelist. Sorry!" -- Your kick message
+
+players.PlayerAdded:Connect(function(player)
+	if not table.find(accounts,player.UserId) then
+		print(player.Name.." attempted to join the game but was not in the whitelist.") -- The script prints a message to indicate to others that an unauthorized player attempted to enter
+		PLAYER:Kick(kick_message) -- The players is kicked
 	end
 end)
